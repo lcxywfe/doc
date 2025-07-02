d18175306c72b6eee0b7f4d6a465260c7c837796
Yao He <lcxywfe@gmail.com>
2025-07-03 00:26:28 +0800

    Create build_3fs_rocky9.sh

---
+++ build_3fs_rocky9.sh
@@ -0,0 +1,89 @@
+#/bin/bash
+
+# REMOTE_IP=192.168.0.72:10000 MGMTD_SERVER_ADDRESSES=RDMA://192.168.0.72:8000 TOKEN="AAD+ioV78QDeStEe2wCnbOhW" ./run.sh
+
+set -e
+
+sudo dnf install -y epel-release
+sudo dnf update -y
+
+sudo dnf install -y cmake
+sudo dnf install -y lz4-devel
+sudo dnf install -y xz-devel
+sudo dnf install -y libdwarf-devel
+sudo dnf install -y libunwind-devel
+sudo dnf install -y libaio-devel
+sudo dnf install -y gflags-devel
+sudo dnf install -y glog-devel
+sudo dnf install -y gtest-devel
+sudo dnf install -y gmock-devel
+sudo dnf install -y clang-tools-extra
+sudo dnf install -y clang
+sudo dnf install -y lld
+sudo dnf install -y gperftools-devel
+sudo dnf install -y gperftools
+sudo dnf install -y openssl-devel
+sudo dnf install -y gcc
+sudo dnf install -y gcc-c++
+sudo dnf install -y boost-devel
+
+sudo dnf config-manager --set-enabled crb
+sudo dnf install -y libuv-devel
+sudo dnf install -y double-conversion-devel
+
+sudo dnf install -y libevent libevent-devel
+
+# fuse
+sudo dnf install -y meson
+# wget https://github.com/libfuse/libfuse/releases/download/fuse-3.16.1/fuse-3.16.1.tar.gz
+tar xzf fuse-3.16.1.tar.gz && cd fuse-3.16.1/
+mkdir build && cd build
+meson setup ..
+ninja
+sudo ninja install
+cd ../../
+
+# foundation db
+# wget https://github.com/apple/foundationdb/releases/download/7.3.63/foundationdb-clients-7.3.63-1.el7.x86_64.rpm
+# wget https://github.com/apple/foundationdb/releases/download/7.3.63/foundationdb-server-7.3.63-1.el7.x86_64.rpm
+sudo dnf install -y foundationdb-clients-7.3.63-1.el7.x86_64.rpm
+sudo dnf install -y foundationdb-server-7.3.63-1.el7.x86_64.rpm
+
+# rust
+export RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup"
+export RUSTUP_UPDATE_ROOT="https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# 3fs
+sudo dnf install -y git
+sudo dnf install -y autoconf
+sudo dnf install -y numactl-devel
+sudo dnf install -y python3-devel
+sudo dnf install -y rdma-core-devel
+[ -f /usr/lib64/libatomic.so ] || ln -s /usr/lib64/libatomic.so.1.2.0 /usr/lib64/libatomic.so
+
+unzip 3FS-clang-19.zip
+cd 3FS
+cmake -S . -B build -DCMAKE_CXX_COMPILER=clang++-19 -DCMAKE_C_COMPILER=clang-19 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build -j 32
+
+sudo python3 setup.py install
+
+# deploy
+sudo mkdir -p /opt/3fs/{bin,etc}
+sudo cp build/bin/hf3fs_fuse_main /opt/3fs/bin
+sudo cp configs/{hf3fs_fuse_main_launcher.toml,hf3fs_fuse_main.toml,hf3fs_fuse_main_app.toml} /opt/3fs/etc
+
+sudo echo ${TOKEN} >/opt/3fs/etc/token.txt
+sudo sed -i "s/^cluster_id.*/cluster_id = \"${CLUSTER_ID:-stage}\"/" /opt/3fs/etc/*
+sudo sed -i "s|mgmtd_server_addresses = \[\]|mgmtd_server_addresses = [\"${MGMTD_SERVER_ADDRESSES//,/\",\"}\"]|g" /opt/3fs/etc/hf3fs_fuse_main_launcher.toml
+sudo sed -i "s|mountpoint = ''|mountpoint = '/3fs'|g" /opt/3fs/etc/hf3fs_fuse_main_launcher.toml
+sudo sed -i "s|token_file = ''|token_file = '/opt/3fs/etc/token.txt'|g" /opt/3fs/etc/hf3fs_fuse_main_launcher.toml
+sudo sed -i "s|remote_ip = ''|remote_ip = \"${REMOTE_IP}\"|g" /opt/3fs/etc/hf3fs_fuse_main.toml
+sudo sed -i "s|mgmtd_server_addresses = \[\]|mgmtd_server_addresses = [\"${MGMTD_SERVER_ADDRESSES//,/\",\"}\"]|g" /opt/3fs/etc/hf3fs_fuse_main.toml
+
+sudo mkdir -p /3fs/
+sudo cp deploy/systemd/hf3fs_fuse_main.service /usr/lib/systemd/system
+sudo systemctl start hf3fs_fuse_main
+df -hT | grep 3fs
